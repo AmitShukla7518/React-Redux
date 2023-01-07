15744844cc910461ac86ef8488d674b9f477df60
Amit Shukla <amit@opskube.com>
2023-01-07 14:35:53 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import HomeContainer from './containers/HomeContainer'
-import HeaderContainer from './containers/HeaderContainer'
-function App() {
-  return (
-    <div className="App">
-      <HeaderContainer />
-     
-      <HomeContainer />
-    </div>
-  );
-}
+ import React from 'react';
+ import logo from './logo.svg';
+ import './App.css';
+ import HomeContainer from './containers/HomeContainer'
+ import HeaderContainer from './containers/HeaderContainer'
+ function App() {
+   return (
+     <div className="App">
+       <HeaderContainer />
+      
+       <HomeContainer />
+     </div>
+   );
+ }
 
-export default App;
+ export default App;
--- src/service/reducers/reducer.js
+++ src/service/reducers/reducer.js
@@ -1,18 +1,17 @@
-import { ADD_TO_CART } from '../constants'
-const initialState = {
-    cardData: []
+import { ADD_TO_CART }  from '../constants'
+const initialState ={
+    cardData:[]
+ }
+export default function cardItems(state = [], action){
+    switch (action.type){
+    case ADD_TO_CART:
+         // console.warn("reducer",action)
+        return [
+            ...state,
+            {cardData: action.data}
+        ]
+    default:   return state
 }
-export default function cardItems(state = [], action) {
-    switch (action.type) {
-        case ADD_TO_CART:
-            // console.warn("reducer",action)
-            return [
-                ...state,
-                {cardData: action.data}
-            ]
-        default:
-            return state
-    }
 
 
-}
+ }
