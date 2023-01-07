3525e5d130460c819650f35b4b48c8e8cfbaf0f9
Amit Shukla <amit@opskube.com>
2023-01-07 14:35:53 +0530

    Second

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,31 @@
-import React from 'react'
+ import React from 'react'
 
-function Home(props) {
-    return (
-        <div>
+ function Home(props) {
+     return (
+         <div>
            
-            <h1>Home Component</h1>
-            <div className="cart-wrapper">
+             <h1>Home Component</h1>
+             <div className="cart-wrapper">
                 <div className="img-wrapper item">
-                    <img src="https://m.media-amazon.com/images/I/71i2XhHU3pL._SX679_.jpg" />
-                </div>
-                <div className="text-wrapper item">
-                    <span>
+                     <img src="https://m.media-amazon.com/images/I/71i2XhHU3pL._SX679_.jpg" />
+                 </div>
+                 <div className="text-wrapper item">
+                     <span>
                         I-Phone
-                    </span>
-                    <span>
+                     </span>
+                     <span>
                         Price: $1000.00
-                    </span>
-                </div>
-                <div className="btn-wrapper item">
-                    <button 
-                    onClick={
-                        ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
-                        }>
+                     </span>
+                 </div>
+                 <div className="btn-wrapper item">
+                     <button 
+                     onClick={
+                         ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
+                         }>
                         Add To Cart</button>
-                </div>
-            </div>
-        </div>
-    )
-}
-export default Home
+                 </div>
+             </div>
+         </div>
+     )
+ }
+ export default Home
